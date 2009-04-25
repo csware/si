9e723b994e235c99c7f66202170977d801037788
Sven Strickroth <email@cs-ware.de>
2009-04-25 01:31:54 +0000

    drop test package

--- source/de/tuclausthal/abgabesystem/util/HibernateSQLExporter.java
+++ source/de/tuclausthal/abgabesystem/util/HibernateSQLExporter.java
@@ -1,9 +1,9 @@
-package de.tuclausthal.abgabesystem.test;
+package de.tuclausthal.abgabesystem.util;
 
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.tool.hbm2ddl.SchemaExport;
 
-public class MyExporter {
+public class HibernateSQLExporter {
 	public static void main(String[] fdf) {
 		SchemaExport bla = new SchemaExport(new AnnotationConfiguration().configure());
 		bla.setOutputFile("1.sql");
