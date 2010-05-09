2355c39f2c0be87cd8e483ad70a0b22428e65a83
Sven Strickroth <email@cs-ware.de>
2010-05-09 18:09:25 +0000

    pretty inline files

--- source/de/tuclausthal/submissioninterface/servlets/view/ShowFileView.java
+++ source/de/tuclausthal/submissioninterface/servlets/view/ShowFileView.java
@@ -29,6 +29,8 @@ import javax.servlet.http.HttpServletResponse;
 import com.uwyn.jhighlight.renderer.Renderer;
 import com.uwyn.jhighlight.renderer.XhtmlRendererFactory;
 
+import de.tuclausthal.submissioninterface.util.Util;
+
 /**
  * View-Servlet for displaying a java-sourcecode file
  * @author Sven Strickroth
@@ -39,25 +41,29 @@ public class ShowFileView extends HttpServlet {
 		String fileName = (String) request.getAttribute("fileName");
 		String code = (String) request.getAttribute("code");
 
+		response.setContentType("text/html");
+		response.setCharacterEncoding("iso-8859-1");
+
 		if (fileName.toLowerCase().endsWith(".java")) {
 			showWithRenderer(response, fileName, code, XhtmlRendererFactory.JAVA);
 		} else if (fileName.toLowerCase().endsWith(".htm") || fileName.toLowerCase().endsWith(".html")) {
 			showWithRenderer(response, fileName, code, XhtmlRendererFactory.HTML);
 		} else if (fileName.toLowerCase().endsWith(".c") || fileName.toLowerCase().endsWith(".cpp")) {
 			showWithRenderer(response, fileName, code, XhtmlRendererFactory.CPP);
-		} else if (fileName.toLowerCase().endsWith("xml")) {
+		} else if (fileName.toLowerCase().endsWith(".xml") || fileName.toLowerCase().endsWith(".classpath") || fileName.toLowerCase().endsWith(".project")) {
 			showWithRenderer(response, fileName, code, XhtmlRendererFactory.XML);
 		} else {
-			response.setContentType("text/plain");
-			response.setCharacterEncoding("iso-8859-1");
 			PrintWriter out = response.getWriter();
-			out.println(code);
+			out.println("<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0 Strict//EN\">");
+			out.println("<html><head><title>" + Util.mknohtml(fileName) + "</title></head><body><h1 style='font-family: sans-serif; font-size: 16pt; font-weight: bold; color: rgb(0,0,0); background: rgb(210,210,210); border: solid 1px black; padding: 5px; text-align: center;'>" + Util.mknohtml(fileName));
+			out.println(" <a href=\"javascript:document.getElementById('code').setAttribute('wrap', 'hard')\">Text umbrechen</a>");
+			out.println("</h1><pre id=code style='font-family: \"Courier New\",Courier,monospace; font-size: 80%'>");
+			out.println(Util.mknohtml(code).replace("\n", "<br>"));
+			out.println("</pre></body></html>");
 		}
 	}
 
 	private void showWithRenderer(HttpServletResponse response, String fileName, String code, String renderertype) throws IOException {
-		response.setContentType("text/html");
-		response.setCharacterEncoding("iso-8859-1");
 		PrintWriter out = response.getWriter();
 		Renderer renderer = XhtmlRendererFactory.getRenderer(renderertype);
 		out.write(renderer.highlight(fileName, code, "iso-8859-1", false));
