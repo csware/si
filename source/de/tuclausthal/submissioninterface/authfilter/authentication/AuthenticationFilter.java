e88d0d9e0f2fa067f165aafb64f6d134bfb12c41
Sven Strickroth <email@cs-ware.de>
2011-10-26 11:58:44 +0200

    Add LoggedIn=false header if not logged in
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- source/de/tuclausthal/submissioninterface/authfilter/authentication/AuthenticationFilter.java
+++ source/de/tuclausthal/submissioninterface/authfilter/authentication/AuthenticationFilter.java
@@ -60,6 +60,7 @@ public class AuthenticationFilter implements Filter {
 		Session session = RequestAdapter.getSession(request);
 		SessionAdapter sa = RequestAdapter.getSessionAdapter(request);
 		if (sa.getUser() == null || (bindToIP && !sa.isIPCorrect(request.getRemoteAddr()))) {
+			response.addHeader("LoggedIn", "false");
 			LoginData logindata = login.getLoginData(request);
 			if (logindata == null) {
 				login.failNoData(request, response);
