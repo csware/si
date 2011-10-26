239f4359406726df9e666b6e2d575a20206e88e9
Sven Strickroth <email@cs-ware.de>
2011-10-26 12:00:02 +0200

    do not show normal upload link if ArgoUML is requested
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- source/de/tuclausthal/submissioninterface/servlets/view/ShowTaskStudentView.java
+++ source/de/tuclausthal/submissioninterface/servlets/view/ShowTaskStudentView.java
@@ -193,8 +193,7 @@ public class ShowTaskStudentView extends HttpServlet {
 			if ("loesung\\.(xmi|zargo|png)".equals(task.getFilenameRegexp())) {
 				out.println("<p><div class=mid><a onclick=\"return confirmLink('ArgoUML öffnen')\" href=\"WebStart?tool=argouml&amp;submissionid=" + submission.getSubmissionid() + "\">ArgoUML öffnen</a></div>");
 				out.println("<script type=\"javascript\">if (!navigator.javaEnabled() || document.applets[0].Version < 1.4){ document.write(\"Sie benötigen mindestens Java 1.6 (JRE), um diese Funktion nutzen zu können. <a href=\"http://www.java.com/\">Download</a>\");</script>");
-			}
-			if ("-".equals(task.getFilenameRegexp()) && task.isShowTextArea() == false) {
+			} else if ("-".equals(task.getFilenameRegexp()) && task.isShowTextArea() == false) {
 				out.println("<div class=mid>Keine Abgabe möglich.</div>");
 			} else if (task.getDeadline().before(Util.correctTimezone(new Date()))) {
 				out.println("<div class=mid>Keine Abgabe mehr möglich.</div>");
@@ -233,8 +232,7 @@ public class ShowTaskStudentView extends HttpServlet {
 			if ("loesung\\.(xmi|zargo|png)".equals(task.getFilenameRegexp())) {
 				out.println("<p><div class=mid><a onclick=\"return confirmLink('ArgoUML öffnen')\" href=\"WebStart?tool=argouml&amp;taskid=" + task.getTaskid() + "\">ArgoUML öffnen</a></div>");
 				out.println("<script type=\"javascript\">if (!navigator.javaEnabled() || document.applets[0].Version < 1.4){ document.write(\"Sie benötigen mindestens Java 1.6 (JRE), um diese Funktion nutzen zu können. <a href=\"http://www.java.com/\">Download</a>\");</script>");
-			}
-			if ("-".equals(task.getFilenameRegexp()) && task.isShowTextArea() == false) {
+			} else if ("-".equals(task.getFilenameRegexp()) && task.isShowTextArea() == false) {
 				out.println("<div class=mid>Keine Abgabe möglich.</div>");
 			} else if (task.getDeadline().before(Util.correctTimezone(new Date()))) {
 				out.println("<div class=mid>Keine Abgabe mehr möglich.</div>");
