d8964642f1fc82d713690cabee04916040516a6d
Sven Strickroth <email@cs-ware.de>
2020-10-24 17:39:29 +0200

    Add missing conversion step in upgrade path
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- sql-update.sql
+++ sql-update.sql
@@ -43,6 +43,9 @@ ALTER TABLE `users` CHANGE `studiengang` `studiengang` VARCHAR(255) CHARACTER SE
 -- 05d34b44a358972099f8f5aa30b060d1b6a6fc55 and e1793a9c9aa944c37135dde82d8dab5e93132def
 ALTER TABLE `logs` ADD `uploadFilename` varchar(255) DEFAULT NULL AFTER `userId`, ADD `upload` longblob DEFAULT NULL AFTER `uploadfilename`;
 
+-- b527092749a8f5e2b718c267d43ef8ff919a8772
+ALTER TABLE `similaritytests` CHANGE `status` `status` TINYINT(4) NOT NULL; 
+
 -- Allow to upload model solution and provide it to students
 ALTER TABLE `tasks` ADD `modelSolutionProvision` VARCHAR(255) AFTER `taskgroupid`;
 
