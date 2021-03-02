47ff55f2e16b687690a79c0fd9a82043eb303bbf
Sven Strickroth <email@cs-ware.de>
2021-03-02 12:33:48 +0100

    Cleanup of JDBC stuff
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/main/java/de/tuclausthal/submissioninterface/util/HibernateListener.java
+++ src/main/java/de/tuclausthal/submissioninterface/util/HibernateListener.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2010, 2020 Sven Strickroth <email@cs-ware.de>
+ * Copyright 2009-2010, 2020-2021 Sven Strickroth <email@cs-ware.de>
  * 
  * This file is part of the SubmissionInterface.
  * 
@@ -18,21 +18,56 @@
 
 package de.tuclausthal.submissioninterface.util;
 
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Enumeration;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.mysql.cj.jdbc.AbandonedConnectionCleanupThread;
 
 public class HibernateListener implements ServletContextListener {
+	final private static Logger LOG = LoggerFactory.getLogger(HibernateListener.class);
 
 	@Override
 	public void contextInitialized(ServletContextEvent event) {
-		HibernateSessionHelper.getSessionFactory(); // Just call the static initializer of that class    
+		LOG.info("Initializing context [{}]", event.getServletContext().getContextPath());
+		HibernateSessionHelper.getSessionFactory(); // Just call the static initializer of that class
+		LOG.info("Initializing context [{}] finished", event.getServletContext().getContextPath());
 	}
 
 	@Override
 	public void contextDestroyed(ServletContextEvent event) {
+		LOG.info("Destroying context [{}]", event.getServletContext().getContextPath());
+
 		HibernateSessionHelper.getSessionFactory().close(); // Free all resources
+		try {
+			Thread.sleep(1500); // C3P0 works highly asynchronous; even closing all threads is done asynchronously; this is not nice, but there does not seem to be any better solution ATM
+		} catch (InterruptedException e) {
+		}
 		AbandonedConnectionCleanupThread.checkedShutdown();
+
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		Enumeration<Driver> drivers = DriverManager.getDrivers();
+		while (drivers.hasMoreElements()) {
+			Driver driver = drivers.nextElement();
+			if (driver.getClass().getClassLoader() != cl) {
+				LOG.debug("Not deregistering JDBC driver \"{}\" as it does not belong to this webapp's ClassLoader", driver.toString());
+				continue;
+			}
+
+			try {
+				LOG.debug("Deregistering JDBC driver \"{}\"", driver.toString());
+				DriverManager.deregisterDriver(driver);
+			} catch (SQLException ex) {
+				LOG.error("Error deregistering JDBC driver \"{}\"", driver.toString());
+			}
+		}
+		LOG.info("Destroying context [{}] finished", event.getServletContext().getContextPath());
 	}
 }
