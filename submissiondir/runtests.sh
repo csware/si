d9174e75f442083e1cb6e6cc45e2962364d923ac
Sven Strickroth <email@cs-ware.de>
2021-04-06 17:52:49 +0200

    Fix c&p error in path
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- submissiondir/runtests.sh
+++ submissiondir/runtests.sh
@@ -7,7 +7,7 @@ if lockfile -! -l 259200 -r 0 "$datadir/gate-runner.lck"; then
 	exit 0
 fi
 date > "$datadir/gate-runner.log"
-if ! java -Dfile.encoding=UTF-8 -Xmx1024m -cp "$webappdir/WEB-INF/classes/:$webappdir/WEB-INF/lib/*" de.tuclausthal.submissioninterface.util.TestRunner "$datadir" >> /var/log/gate-runner.log 2>&1; then
+if ! java -Dfile.encoding=UTF-8 -Xmx1024m -cp "$webappdir/WEB-INF/classes/:$webappdir/WEB-INF/lib/*" de.tuclausthal.submissioninterface.util.TestRunner "$datadir" >> "$datadir/gate-runner.log" 2>&1; then
 	cat "$datadir/gate-runner.log"
 fi
 date >> "$datadir/gate-runner.log"
