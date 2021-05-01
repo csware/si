4f314dd815fd2f3a5f462b0c58dc777cb569170d
Sven Strickroth <email@cs-ware.de>
2021-05-01 19:26:17 +0200

    Add support for parallel deployments
    cf. https://tomcat.apache.org/tomcat-9.0-doc/config/context.html#Parallel_deployment
    
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- submissiondir/runtests.sh
+++ submissiondir/runtests.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-webappdir="/var/lib/tomcat9/webapps/submissionsystem"
+contextname="submissionsystem"
+webappdir="/var/lib/tomcat9/webapps/"
+contextpath="$webappdir$contextname"
 datadir="/srv/submissioninterface"
+
+if [[ ! -d "$contextpath" ]]; then
+	latestcontextpath=$(find "$webappdir" -maxdepth 1 -type d -name "$contextname##*" | sort -r | head -n1)
+	if [[ -z "$latestcontextpath" ]]; then
+		echo "\"$contextpath\" not found" >&2
+		exit 1
+	fi
+	contextpath="$latestcontextpath"
+fi
 
 if lockfile -! -l 259200 -r 0 "$datadir/gate-runner.lck"; then
 	exit 0
 fi
 date > "$datadir/gate-runner.log"
-if ! java -Dfile.encoding=UTF-8 -Xmx1024m -cp "$webappdir/WEB-INF/classes/:$webappdir/WEB-INF/lib/*" de.tuclausthal.submissioninterface.util.TestRunner "$datadir" >> "$datadir/gate-runner.log" 2>&1; then
+if ! java -Dfile.encoding=UTF-8 -Xmx1024m -cp "$contextpath/WEB-INF/classes/:$contextpath/WEB-INF/lib/*" de.tuclausthal.submissioninterface.util.TestRunner "$datadir" >> "$datadir/gate-runner.log" 2>&1; then
 	cat "$datadir/gate-runner.log"
 fi
 date >> "$datadir/gate-runner.log"
