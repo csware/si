67578a7b3551a157f94cdb3b9f4b24c1045d5e86
Sven Strickroth <email@cs-ware.de>
2022-01-20 12:23:44 +0100

    Improve guessing task time intervals when creating new tasks
    Fixes a possible null pointer exception.
    
    Signed-off-by: Sven Strickroth <email@cs-ware.de>

--- src/main/java/de/tuclausthal/submissioninterface/servlets/controller/TaskManager.java
+++ src/main/java/de/tuclausthal/submissioninterface/servlets/controller/TaskManager.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009-2012, 2014, 2020-2021 Sven Strickroth <email@cs-ware.de>
+ * Copyright 2009-2012, 2014, 2020-2022 Sven Strickroth <email@cs-ware.de>
  * 
  * This file is part of the SubmissionInterface.
  * 
@@ -128,10 +128,12 @@ public class TaskManager extends HttpServlet {
 					task.setStart(potentialTaskGroup.getTasks().get(0).getStart());
 					task.setDeadline(potentialTaskGroup.getTasks().get(0).getDeadline());
 					task.setShowPoints(potentialTaskGroup.getTasks().get(0).getShowPoints());
-				} else if (lecture.getTaskGroups().size() > 1 && (potentialTaskGroup = lecture.getTaskGroups().get(lecture.getTaskGroups().size() - 2)) != null) {
+				} else if (lecture.getTaskGroups().size() > 1 && (potentialTaskGroup = lecture.getTaskGroups().get(lecture.getTaskGroups().size() - 2)) != null && !potentialTaskGroup.getTasks().isEmpty()) {
 					task.setStart(potentialTaskGroup.getTasks().get(0).getStart().plusWeeks(1));
 					task.setDeadline(potentialTaskGroup.getTasks().get(0).getDeadline().plusWeeks(1));
-					task.setShowPoints(potentialTaskGroup.getTasks().get(0).getShowPoints().plusWeeks(1));
+					if (potentialTaskGroup.getTasks().get(0).getShowPoints() != null) {
+						task.setShowPoints(potentialTaskGroup.getTasks().get(0).getShowPoints().plusWeeks(1));
+					}
 				} else {
 					task.setStart(ZonedDateTime.now());
 					task.setDeadline(ZonedDateTime.now().plusWeeks(1));
