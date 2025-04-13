051b48ac57dd47077e72c8804316d88f9012b9ee
Christian Wagner <christian.w.github@gmail.com>
2025-04-13 23:49:40 +0200

    Add new HaskellRuntimeTest in testframework and in datamodel
    - extends DockerTest

---
+++ src/main/java/de/tuclausthal/submissioninterface/persistence/datamodel/HaskellRuntimeTest.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2025 Sven Strickroth <email@cs-ware.de>
+ * Copyright 2025 Christian Wagner <christian.wagner@campus.lmu.de>
+ *
+ * This file is part of the GATE.
+ *
+ * GATE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as
+ * published by the Free Software Foundation.
+ *
+ * GATE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GATE. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.tuclausthal.submissioninterface.persistence.datamodel;
+
+import de.tuclausthal.submissioninterface.testframework.tests.AbstractTest;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Transient;
+
+
+/**
+ * Haskell runtime test, extends the DockerTest by automatically generating haskell testcases and by clustering
+ * @author Christian Wagner
+ */
+@Entity
+public class HaskellRuntimeTest extends DockerTest {
+	@Override
+	@Transient
+	public AbstractTest<DockerTest> getTestImpl() {
+		return new de.tuclausthal.submissioninterface.testframework.tests.impl.HaskellRuntimeTest(this);
+	}
+}
