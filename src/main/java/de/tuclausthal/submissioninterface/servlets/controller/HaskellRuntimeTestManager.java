b2d66ff6426e5a007dc6c13731caaf033e84c581
Christian Wagner <christian.w.github@gmail.com>
2025-04-14 08:24:30 +0200

    Implement view and controller for haskell runtime test
    - view: HaskellRuntimeTestManagerView (based on DockerTestManagerOverView)
    - controller: HaskellRuntimeTestManager (based on DockerTestManager)

--- src/main/java/de/tuclausthal/submissioninterface/servlets/controller/HaskellRuntimeTestManager.java
+++ src/main/java/de/tuclausthal/submissioninterface/servlets/controller/HaskellRuntimeTestManager.java
@@ -19,20 +19,33 @@
 
 package de.tuclausthal.submissioninterface.servlets.controller;
 
+import de.tuclausthal.submissioninterface.persistence.dao.DAOFactory;
+import de.tuclausthal.submissioninterface.persistence.dao.ParticipationDAOIf;
+import de.tuclausthal.submissioninterface.persistence.dao.TestDAOIf;
+import de.tuclausthal.submissioninterface.persistence.datamodel.Test;
+import de.tuclausthal.submissioninterface.persistence.datamodel.HaskellRuntimeTest;
+import de.tuclausthal.submissioninterface.persistence.datamodel.DockerTestStep;
+import de.tuclausthal.submissioninterface.persistence.datamodel.Participation;
+import de.tuclausthal.submissioninterface.persistence.datamodel.ParticipationRole;
 import de.tuclausthal.submissioninterface.servlets.GATEController;
 import de.tuclausthal.submissioninterface.servlets.RequestAdapter;
 import de.tuclausthal.submissioninterface.servlets.view.HaskellRuntimeTestManagerView;
+import de.tuclausthal.submissioninterface.servlets.view.MessageView;
+import de.tuclausthal.submissioninterface.util.Util;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.io.IOException;
 import java.io.Serial;
+import java.util.Objects;
 
 /**
- * Controller-Servlet for clustering haskell submissions based on common errors (dynamic/runtime analysis)
+ * Controller-Servlet for clustering haskell submissions based on common errors (dynamic/runtime analysis).
+ * This servlet allows advisors to manage (add, edit, remove) test steps.
  *
  * @author Christian Wagner
  */
@@ -43,12 +56,105 @@ public class HaskellRuntimeTestManager extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        // similar code in DockerTestManager
         Session session = RequestAdapter.getSession(request);
 
-        // TODO@CHW missing implementation, request redirected by TestManager
+        TestDAOIf testDAOIf = DAOFactory.TestDAOIf(session);
+        Test test = testDAOIf.getTest(Util.parseInteger(request.getParameter("testid"), 0));
+        if (!(test instanceof HaskellRuntimeTest haskellRuntimeTest)) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            request.setAttribute("title", "Test nicht gefunden");
+            getServletContext().getNamedDispatcher(MessageView.class.getSimpleName()).forward(request, response);
+            return;
+        }
 
-        request.setAttribute("testattribute", "somevalue");
+        ParticipationDAOIf participationDAO = DAOFactory.ParticipationDAOIf(session);
+        Participation participation = participationDAO.getParticipation(RequestAdapter.getUser(request), haskellRuntimeTest.getTask().getTaskGroup().getLecture());
+        if (participation == null || participation.getRoleType() != ParticipationRole.ADVISOR) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "insufficient rights");
+            return;
+        }
+
+        request.setAttribute("test", haskellRuntimeTest);
         getServletContext().getNamedDispatcher(HaskellRuntimeTestManagerView.class.getSimpleName()).forward(request, response);
+    }
 
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        // similar code in DockerTestManager
+        Session session = RequestAdapter.getSession(request);
+        TestDAOIf testDAOIf = DAOFactory.TestDAOIf(session);
+        Test test = testDAOIf.getTest(Util.parseInteger(request.getParameter("testid"), 0));
+        if (!(test instanceof HaskellRuntimeTest haskellRuntimeTest)) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            request.setAttribute("title", "Test nicht gefunden");
+            getServletContext().getNamedDispatcher(MessageView.class.getSimpleName()).forward(request, response);
+            return;
+        }
+
+        ParticipationDAOIf participationDAO = DAOFactory.ParticipationDAOIf(session);
+        Participation participation = participationDAO.getParticipation(RequestAdapter.getUser(request), haskellRuntimeTest.getTask().getTaskGroup().getLecture());
+        if (participation == null || participation.getRoleType() != ParticipationRole.ADVISOR) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "insufficient rights");
+            return;
+        }
+
+        if ("edittest".equals(request.getParameter("action"))) {
+            Transaction tx = session.beginTransaction();
+            haskellRuntimeTest.setTestTitle(request.getParameter("title"));
+            haskellRuntimeTest.setForTutors(request.getParameter("tutortest") != null);
+            haskellRuntimeTest.setTimesRunnableByStudents(Util.parseInteger(request.getParameter("timesRunnableByStudents"), 0));
+            haskellRuntimeTest.setGiveDetailsToStudents(request.getParameter("giveDetailsToStudents") != null);
+            haskellRuntimeTest.setPreparationShellCode(request.getParameter("preparationcode").replaceAll("\r\n", "\n")); // TODO@CHW: can getParameter("preparationcode") be null?
+            tx.commit();
+            response.sendRedirect(Util.generateRedirectURL(HaskellRuntimeTestManager.class.getSimpleName() + "?testid=" + haskellRuntimeTest.getId(), response));
+            return;
+        } else if ("addNewStep".equals(request.getParameter("action"))) {
+            String title = request.getParameter("title");
+            String testCode = request.getParameter("testcode").replaceAll("\r\n", "\n");
+            String expect = request.getParameter("expect").replaceAll("\r\n", "\n");
+            DockerTestStep newStep = new DockerTestStep(haskellRuntimeTest, title, testCode, expect);
+            Transaction tx = session.beginTransaction();
+            session.persist(newStep);
+            tx.commit();
+            response.sendRedirect(Util.generateRedirectURL(HaskellRuntimeTestManager.class.getSimpleName() + "?testid=" + haskellRuntimeTest.getId(), response));
+            return;
+        } else if ("updateStep".equals(request.getParameter("action"))) {
+            DockerTestStep step = null;
+            for (DockerTestStep istep : haskellRuntimeTest.getTestSteps()) {
+                if (istep.getTeststepid() == Util.parseInteger(request.getParameter("teststepid"), -1)) {
+                    step = istep;
+                    break;
+                }
+            }
+            if (step != null) {
+                Transaction tx = session.beginTransaction();
+                String title = request.getParameter("title");
+                String testCode = request.getParameter("testcode").replaceAll("\r\n", "\n");
+                step.setTitle(title);
+                step.setTestcode(testCode);
+                step.setExpect(Objects.toString(request.getParameter("expect"), "").replaceAll("\r\n", "\n"));
+                tx.commit();
+            }
+            response.sendRedirect(Util.generateRedirectURL(HaskellRuntimeTestManager.class.getSimpleName() + "?testid=" + haskellRuntimeTest.getId(), response));
+            return;
+        } else if ("deleteStep".equals(request.getParameter("action"))) {
+            DockerTestStep step = null;
+            for (DockerTestStep istep : haskellRuntimeTest.getTestSteps()) {
+                if (istep.getTeststepid() == Util.parseInteger(request.getParameter("teststepid"), -1)) {
+                    step = istep;
+                    break;
+                }
+            }
+            if (step != null) {
+                Transaction tx = session.beginTransaction();
+                session.remove(step);
+                tx.commit();
+            }
+            response.sendRedirect(Util.generateRedirectURL(HaskellRuntimeTestManager.class.getSimpleName() + "?testid=" + haskellRuntimeTest.getId(), response));
+            return;
+        }
+
+        response.sendError(HttpServletResponse.SC_METHOD_NOT_ALLOWED, "invalid request");
     }
 }
--- src/main/java/de/tuclausthal/submissioninterface/servlets/view/HaskellRuntimeTestManagerView.java
+++ src/main/java/de/tuclausthal/submissioninterface/servlets/view/HaskellRuntimeTestManagerView.java
@@ -20,9 +20,14 @@
 package de.tuclausthal.submissioninterface.servlets.view;
 
 
+import de.tuclausthal.submissioninterface.persistence.datamodel.HaskellRuntimeTest;
+import de.tuclausthal.submissioninterface.persistence.datamodel.DockerTestStep;
 import de.tuclausthal.submissioninterface.servlets.GATEView;
+import de.tuclausthal.submissioninterface.servlets.controller.HaskellRuntimeTestManager;
+import de.tuclausthal.submissioninterface.servlets.controller.TaskManager;
 import de.tuclausthal.submissioninterface.template.Template;
 import de.tuclausthal.submissioninterface.template.TemplateFactory;
+import de.tuclausthal.submissioninterface.util.Util;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -48,14 +53,161 @@ public class HaskellRuntimeTestManagerView extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         Template template = TemplateFactory.getTemplate(request, response);
 
+        HaskellRuntimeTest test = (HaskellRuntimeTest) request.getAttribute("test");
+
         template.addKeepAlive();
-        template.printTemplateHeader("Haskell dynamisches Error Clustering bearbeiten");
+        template.printEditTaskTemplateHeader("Haskell Runtime Test bearbeiten", test.getTask());
 
         PrintWriter out = response.getWriter();
-        out.println("...");
-        // printHaskellDynamicClusteringTestForm(); // TODO@CHW needs task as parameter, look at Docker test implementation
 
-        // TODO@CHW implement HTML to setup the haskell dynamic clustering in detail
+        // similar code in TestManagerAddTestFormView
+        // TODO@CHW: can this code duplicate be avoided using printHaskellRuntimeTestForm()?
+        // TODO@CHW: compare with printHaskellRuntimeTestForm() and add remaining fields
+        out.println("<h2>" + Util.escapeHTML(test.getTestTitle()) + "</h2>");
+        out.println("<form action=\"" + Util.generateHTMLLink("?", response) + "\" method=post>");
+        out.println("<input type=hidden name=testid value=\"" + test.getId() + "\">");
+        out.println("<input type=hidden name=action value=edittest>");
+        out.println("<table>");
+        out.println("<tr>");
+        out.println("<th>Titel:</th>");
+        out.println("<td><input type=text name=title maxlength=250 size=60 value=\"" + Util.escapeHTML(test.getTestTitle()) + "\" required></td>");
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("<th>Tutorentest:</th>");
+        out.println("<td><input type=checkbox name=tutortest" + (test.isForTutors() ? " checked" : "") + "> (Ergebnis wird den TutorInnen zur Korrektur angezeigt)</td>");
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("<th># ausführbar für Studierende:</th>");
+        out.println("<td><input type=text name=timesRunnableByStudents value=\"" + test.getTimesRunnableByStudents() + "\" required=required pattern=\"[0-9]+\"></td>");
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("<th>Studierenden Test-Details anzeigen:</th>");
+        out.println("<td><input type=checkbox name=giveDetailsToStudents" + (test.isGiveDetailsToStudents() ? " checked" : "") + "></td>");
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("<th>Preparation Code:</th>");
+        out.println("<td><textarea cols=60 rows=10 name=preparationcode>" + Util.escapeHTML(test.getPreparationShellCode()) + "</textarea></td>");
+        out.println("</tr>");
+        out.println("<tr>");
+        out.print("<td colspan=2 class=mid><input type=submit value=speichern> <a href=\"");
+        out.print(Util.generateHTMLLink(TaskManager.class.getSimpleName() + "?action=editTask&taskid=" + test.getTask().getTaskid() + "&lecture=" + test.getTask().getTaskGroup().getLecture().getId(), response));
+        out.println("\">Abbrechen</a></td>");
+        out.println("</tr>");
+        out.println("</table>");
+        out.println("</form>");
+
+        out.println("<hr>");
+        out.println("<h2>Testschritte</h2>");
+
+        for (DockerTestStep step : test.getTestSteps()) {
+            out.println("<h3>" + Util.escapeHTML(step.getTitle()) + "</h2>");
+            out.println("<form action=\"" + Util.generateHTMLLink("?", response) + "\" method=post>");
+            out.println("<input type=hidden name=testid value=\"" + test.getId() + "\">");
+            out.println("<input type=hidden name=teststepid value=\"" + step.getTeststepid() + "\">");
+            out.println("<input type=hidden name=action value=updateStep>");
+            out.println("<table>");
+            out.println("<tr>");
+            out.println("<th>Titel:</th>");
+            out.println("<td><input type=text name=title maxlength=250 size=60 value=\"" + Util.escapeHTML(step.getTitle()) + "\" required></td>");
+            out.println("</tr>");
+            out.println("<tr>");
+            out.println("<th>Testcode:</th>");
+            out.println("<td><textarea cols=60 rows=10 name=testcode required>" + Util.escapeHTML(step.getTestcode()) + "</textarea></td>");
+            out.println("</tr>");
+            out.println("<tr>");
+            out.println("<th>Erwartete Ausgabe:</th>");
+            out.println("<td><textarea cols=60 rows=10 name=expect>" + Util.escapeHTML(step.getExpect()) + "</textarea></td>");
+            out.println("</tr>");
+            out.println("<tr>");
+            out.println("<td colspan=2 class=mid><input type=submit value=speichern></td>");
+            out.println("</tr>");
+            out.println("<tr>");
+            out.println("<td colspan=2 class=mid>");
+            out.print("<a onclick=\"return sendAsPost(this, 'Wirklich löschen?')\" href=\"");
+            out.print(Util.generateHTMLLink(HaskellRuntimeTestManager.class.getSimpleName() + "?testid=" + test.getId() + "&action=deleteStep&teststepid=" + step.getTeststepid(), response));
+            out.println("\">Löschen</a></td>");
+            out.println("</tr>");
+            out.println("</table>");
+            out.println("</form>");
+        }
+
+        out.println("<hr>");
+
+        out.println("<h3>Neuer Test-Schritt <a href=\"#\" onclick=\"toggleVisibility('teststephelp'); return false;\">(?)</a></h2>");
+
+        out.println("<div style=\"display:none;\" id=teststephelp><b>Hilfe:</b><br>");
+        out.println("<p>Diese Art von Test erlaubt es beliebige einfache Ausgabe-Tests zu definieren. Mit dem Preparation-Code können vorbereitende Schritte als Bash-Skript programmiert werden. Ist dieser Schritt erfolgreich, werden die einzelnen Testschritte nacheinander aufgerufen, wobei für jeden Testschritt die Ausgabe auf STDOUT mit einem erwartetem Wert überprüft werden.</p>");
+        out.println("<p>Preparation-Code z. B.:<br><textarea cols=80 rows=2 name=testcode disabled>echo -e \"hallo servus\\nsehr sakrisch\\ngut guad\" > woerterbuch.txt\nghc uebersetzer-a.hs</textarea></p>");
+        /* @formatter:off */
+        out.println("<p>Test-Schritt-Definition z. B.:<br>"+
+                "<table>" +
+                "<tr>" +
+                "<th>Titel:</th>" +
+                "<td><input type=text name=title disabled size=60 value=\"Eingabe &quot;gut&quot;\"></td>" +
+                "</tr>" +
+                "<tr>" +
+                "<th>Testcode:</th>" +
+                "<td><textarea cols=60 rows=1 name=testcode disabled>echo \"gut\" | ./uebersetzer-a</textarea></td>" +
+                "</tr>" +
+                "<tr>" +
+                "<th>Erwartete Ausgabe:</th>" +
+                "<td><textarea cols=60 rows=2 name=expect disabled>Geben Sie ein hochdeutsches Wort ein:\n"
+                + "\"guad\"</textarea></td>" +
+                "</tr>" +
+                "</table></p>");
+        out.println("<p>Ausgabe bei Testdurchführung z. B.:<br>" +
+                "<table>" +
+                "<tr>" +
+                "<th>Test</th>" +
+                "<th>Erwartet</th>" +
+                "<th>Erhalten</th>" +
+                "<th>OK?</th>" +
+                "</tr>" +
+                "<tr>" +
+                "<td>Eingabe \"gut\"</td>" +
+                "<td><pre>Geben Sie ein hochdeutsches Wort ein:\n"
+                + "\"guad\"</pre></td>" +
+                "<td><pre>Geben Sie ein hochdeutsches Wort ein:\n"
+                + "\"guad\"" +
+                "</pre></td>" +
+                "<td><span class=green>ja</span></td>" +
+                "</tr>" +
+                "<tr>" +
+                "<td>Leere Eingabe</td>" +
+                "<td><pre></pre></td>" +
+                "<td><pre></pre></td>" +
+                "<td><span class=green>ja</span></td>" +
+                "</tr>" +
+                "</table></p>");
+        /* @formatter:on */
+        out.println("<p>Die erwartete Ausgabe und tatsächliche Ausgabe wird getrimmt und hinsichtlich der Zeilenenden auf \"\\n\" normalisiert und mittels exaktem Stringvergleich verglichen. Im Testcode kann beliebiger Bash-Code verwendet werden. In der Umgebung ist per Default \"set -e\" gesetzt, so dass das Skript nach einem nicht behandelten Fehler sofort abgebrochen wird.</p>");
+        out.println("<p>Wird das Bash-Skript vorzeitig beendet, erhalten die Studierenden die Ausgabe \"Nicht alle Tests wurden durchlaufen. Das Program wurde nicht ordentlich beendet.\", wobei die Tabelle alle bisherigen zzgl. den zuletzt ausgeführten Test zeigt (die Spalte \"Erhalten\" ist dann ggf. leer). Bricht das Testskript nach der Preparation-Code-Phase ab, wird dies den Studierenden als Laufzeitfehler angezeigt und der Inhalt von STDERR seit dem Beginn des Testschritts bereitgestellt. Bricht das Testskript beim Preparationcode mit dem ExitCode 15 ab, wird den Studierenden nur der Text \"Der zu testende Code ist syntaktisch nicht korrekt und kann daher nicht getestet werden.\" angezeigt, ansonsten, dass ein Syntaxfehler aufgetreten ist inkl. Inhalt von STDERR.</p>");
+        out.println("<p>Diese Art von Test ist für einfache Ausgabetests ausgerichtet, es können aber auch approximative Tests oder nahezu beliebige Überprüfungen durchgefühert werden, z.B. erwartet \"True\" und Testcode \"ghci -e \"pi_approx 6 < 3.0 && pi_approx 6 > 2.99\" pi_approx.hs\".</p>");
+        out.println("</div>");
+
+        out.println("<form action=\"" + Util.generateHTMLLink("?", response) + "\" method=post>");
+        out.println("<input type=hidden name=testid value=\"" + test.getId() + "\">");
+        out.println("<input type=hidden name=action value=addNewStep>");
+        out.println("<table>");
+        out.println("<tr>");
+        out.println("<th>Titel:</th>");
+        out.println("<td><input type=text name=title maxlength=250 value=\"\" size=60 required></td>");
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("<th>Testcode:</th>");
+        out.println("<td><textarea cols=60 rows=10 name=testcode required></textarea></td>");
+        out.println("</tr>");
+        out.println("<tr>");
+        out.println("<th>Erwartete Ausgabe:</th>");
+        out.println("<td><textarea cols=60 rows=10 name=expect></textarea></td>");
+        out.println("</tr>");
+        out.println("<tr>");
+        out.print("<td colspan=2 class=mid><input type=submit value=speichern> <a href=\"");
+        out.print(Util.generateHTMLLink(TaskManager.class.getSimpleName() + "?action=editTask&taskid=" + test.getTask().getTaskid() + "&lecture=" + test.getTask().getTaskGroup().getLecture().getId(), response));
+        out.println("\">Abbrechen</a></td>");
+        out.println("</tr>");
+        out.println("</table>");
+        out.println("</form>");
 
         template.printTemplateFooter();
     }
